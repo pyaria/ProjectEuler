255c12e6533539c1168a7c1f5e531903a2bba55d
Jade Tong <jadetong@gmail.com>
2018-10-20 01:26:01 -0700

    Problem 1 in c#:

--- ProjectEuler/ProjectEuler/Program.cs
+++ ProjectEuler/ProjectEuler/Program.cs
@@ -23,7 +23,8 @@ namespace ProjectEuler
             switch (problemNo)
             {
                 case 1: return $@"If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
-\nThe sum of these multiples is 23.\nFind the sum of all the multiples of 3 or 5 below 1000.";
+The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.";
                 default: return $"No description has been entered for problem #{problemNo} yet";
             }
         }
@@ -34,15 +35,38 @@ namespace ProjectEuler
             {
                 case 1:
                     {
-                        return RunMultiplesOf3And5();
+                        return RunProblem1();
                     }
                 default: return "Problem not yet solved/implemented/in system";
             }
         }
 
-        private static int RunMultiplesOf3And5()
+        private static int RunProblem1()
         {
-            throw new NotImplementedException();
+            var multiplesOf3 = GetMultiplesBelowANumber(3, 1000);
+            Console.WriteLine(string.Join(", ", multiplesOf3));
+            var multiplesOf5 = GetMultiplesBelowANumber(5, 1000);
+            Console.WriteLine(string.Join(", ", multiplesOf5));
+            var distinct = multiplesOf3.Concat(multiplesOf5).Distinct();
+            return distinct.Sum();
+        }
+        private static int MultipleNumbers(IEnumerable<int> numbers)
+        {
+            var multiple = numbers.First();
+            foreach (var number in numbers.Skip(1))
+            {
+                multiple *= number;
+            }
+            return multiple;
+        }
+        private static IEnumerable<int> GetMultiplesBelowANumber(int multiplesOfNumber, int belowThisNumber)
+        {
+            var i = 1;
+            while (i * multiplesOfNumber < belowThisNumber)
+            {
+                yield return i * multiplesOfNumber;
+                i++;
+            }
         }
     }
 }
