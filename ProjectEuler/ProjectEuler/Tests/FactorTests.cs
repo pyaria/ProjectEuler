ac0843f08e402969fabf26be47e08fac70e90c96
Jade Tong <jadetong@gmail.com>
2018-10-21 18:40:29 -0700

    Add Tests Xunit

---
+++ ProjectEuler/ProjectEuler/Tests/FactorTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProjectEuler.Tests
+{
+    public class FactorTests
+    {
+        [Fact]
+        public void FactorTest()
+        {
+            Assert.Equal("true", "false");
+        }
+        [Fact]
+        public void NegativeTest()
+        {
+            Assert.False(true);
+        }
+    }
+}
